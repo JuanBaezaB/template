8b12096c43d25d2916925e273a03b15fa3beec20
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2023-02-10 16:37:04 -0300

    profilePhotos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,6 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
